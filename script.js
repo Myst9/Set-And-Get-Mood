d690231e38e2b8e7039a2643629252b82f96f15a
varshitha3 <srivarshithabodala@gmail.com>
2023-07-26 15:53:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const MoodContractAddress = "0x1ea400f0421b7c6503690E441164F954aE724047";
+const MoodContractABI = [
+{
+"inputs": [
+    {
+        "internalType": "string",
+        "name": "_mood",
+        "type": "string"
+    }
+],
+"name": "setMood",
+"outputs": [],
+"stateMutability": "nonpayable",
+"type": "function"
+},
+{
+"inputs": [],
+"name": "getMood",
+"outputs": [
+    {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+    }
+],
+"stateMutability": "view",
+"type": "function"
+}
+];
+
+let MoodContract = undefined;
+let signer = undefined;
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "sepolia");
+
+provider.send("eth_requestAccounts", []).then(() => {
+    provider.listAccounts().then((accounts) => {
+      signer = provider.getSigner(accounts[0]);
+      MoodContract = new ethers.Contract(
+        MoodContractAddress,
+        MoodContractABI,
+        signer
+      );
+    });
+});
+
+async function getMood() {
+    const mood = await MoodContract.getMood();
+    document.getElementById("showMood").innerText = `Your Mood: ${mood}`;
+    console.log(mood);
+}
+
+async function setMood() {
+    const mood = document.getElementById("mood").value;
+    await MoodContract.setMood(mood);
+}
